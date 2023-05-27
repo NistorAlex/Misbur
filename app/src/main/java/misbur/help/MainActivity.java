759dcb4dce0a51789cb2ddd85e7e3026e39e191d
NistorAlex <nistor.alex1244@gmail.com>
2023-05-27 23:11:37 +0300

    first

---
+++ app/src/main/java/misbur/help/MainActivity.java
@@ -0,0 +1,49 @@
+package misbur.help;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.ktx.Firebase;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
+    Button button;
+    TextView textView;
+    FirebaseUser user;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activityintro);
+
+        auth = FirebaseAuth.getInstance();
+        button = findViewById(R.id.btn_logout);
+        user = auth.getCurrentUser();
+        textView = findViewById(R.id.user_details);
+        if ( user == null){
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        else {
+            textView.setText(user.getEmail());
+        }
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
